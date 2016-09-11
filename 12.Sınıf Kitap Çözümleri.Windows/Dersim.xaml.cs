f248cde2ee60c8316d13d497da18511ef10a0107
gookce <gookcedemir@gmail.com>
2016-09-11 12:38:12 +0300

    done

---
+++ 12.Sınıf Kitap Çözümleri.Windows/Dersim.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace _12.Sınıf_Kitap_Çözümleri
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Dersim : Page
+    {
+        public static string Dersinadı;
+        public Dersim()
+        {
+            this.Loaded+=Dersim_Loaded;
+            this.InitializeComponent();
+        }
+        string[] arrayyeni = new string[] { };
+        string dosyabasi;
+        int index;
+
+        async void Dersim_Loaded(object sender,RoutedEventArgs e)
+        {
+            txtDers.Text = Dersinadı;
+            var _Folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            _Folder = await _Folder.GetFolderAsync("Assets");
+            var _File = await _Folder.GetFileAsync(MainPage.Derslerim);
+            txtIcerik.Text = await Windows.Storage.FileIO.ReadTextAsync(_File, Windows.Storage.Streams.UnicodeEncoding.Utf8);
+
+
+            index = MainPage.Comboboxindex;
+           
+            if (Dersinadı == "Dil ve Anlatım")
+            {
+                dosyabasi = "dil ";
+                arrayyeni = MainPage.DilAnlatım;
+
+            }
+            else if (Dersinadı == "Edebiyat")
+            {
+
+                dosyabasi = "edb ";
+                arrayyeni = MainPage.Edebiyat;
+            }
+
+            comboboxindexcheck();
+        }
+
+        private void comboboxindexcheck()
+        {
+
+            if (index == 0)
+            {
+                Btngeri.IsEnabled = false;
+                Btngeri.Visibility = Visibility.Collapsed;
+            }
+            else if (index == (MainPage.Comboboxlenght - 1))
+            {
+                btnileri.IsEnabled = false;
+                btnileri.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                Btngeri.IsEnabled = true;
+                Btngeri.Visibility = Visibility.Visible;
+                btnileri.IsEnabled = true;
+                btnileri.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void btnAnaSayf_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+
+
+
+        private async void Btngeri_Click(object sender, RoutedEventArgs e)
+        {
+            index = index - 1;
+            var _Folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            _Folder = await _Folder.GetFolderAsync("Assets");
+            var _File = await _Folder.GetFileAsync(dosyabasi + arrayyeni[index] + ".txt");
+            txtIcerik.Text = await Windows.Storage.FileIO.ReadTextAsync(_File, Windows.Storage.Streams.UnicodeEncoding.Utf8);
+
+
+            comboboxindexcheck();
+        }
+
+        private async void btnileri_Click(object sender, RoutedEventArgs e)
+        {
+            index = index + 1;
+            var _Folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            _Folder = await _Folder.GetFolderAsync("Assets");
+            var _File = await _Folder.GetFileAsync(dosyabasi + arrayyeni[index] + ".txt");
+            txtIcerik.Text = await Windows.Storage.FileIO.ReadTextAsync(_File, Windows.Storage.Streams.UnicodeEncoding.Utf8);
+
+
+            comboboxindexcheck();
+        }
+
+        
+    }
+}
---
+++ 12.Sınıf Kitap Çözümleri.Windows/MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace _12.Sınıf_Kitap_Çözümleri
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            btngit.Visibility = Visibility.Collapsed;
+            Combobox.IsEnabled = false;
+        }
+        public static string Derslerim;
+        public static int Comboboxindex;
+        public static int Comboboxlenght;
+        public static string[] Edebiyat = { "10-11", "17-18", "21-23", "25-29", "31-34", "37-39", "41-48", "57-62", "76-79", "80-86", "109-113", "116-119", "122-128", "130-138", "139-150", "155-159", "163-171", "175-185", "192-221" };
+        public static string[] DilAnlatım = { "10-18", "21-29", "40-54","68-73", "78-93", "103-113", "115-133", "141-156", "165-172", "178-223" };
+        public static string dersinuzantısı;
+        private void btnEdb_Click(object sender, RoutedEventArgs e)
+        {
+            Dersim.Dersinadı = "Edebiyat";
+            dersinuzantısı="edb";
+            Combobox.Items.Clear();
+
+            for(int i=0;i<19;i++)
+            {
+                Combobox.Items.Add(Edebiyat[i]);
+            }
+            Combobox.IsEnabled = true;
+            
+            this.btnEdb.Background = new SolidColorBrush(Windows.UI.Colors.Black);
+            this.btnEdb.Background.Opacity = 0.2;
+            this.btnDil.Background.Opacity = 0;
+        }
+        private void btnDil_Click(object sender, RoutedEventArgs e)
+        {
+            Dersim.Dersinadı = "Dil ve Anlatım";
+            dersinuzantısı= "dil";
+            Combobox.Items.Clear();
+            
+
+            for(int i=0;i<10;i++)
+            {
+                Combobox.Items.Add(DilAnlatım[i]);
+            }
+            Combobox.IsEnabled = true;
+            
+            this.btnDil.Background = new SolidColorBrush(Windows.UI.Colors.Black);
+            this.btnDil.Background.Opacity = 0.2;
+            this.btnEdb.Background.Opacity = 0;
+        }
+        private void Combobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(Combobox.SelectedIndex>=0)
+            {
+                btngit.Visibility = Visibility.Visible;
+                btngit.IsEnabled = true;
+            }
+            else
+            {
+                btngit.Visibility = Visibility.Collapsed;
+                btngit.IsEnabled = false;
+            }
+        }
+        private void btngit_Click(object sender, RoutedEventArgs e)
+        {
+          Derslerim=dersinuzantısı+" "+Combobox.SelectedItem.ToString()+".txt";
+          Comboboxindex = Combobox.SelectedIndex;
+          Comboboxlenght = Combobox.Items.Count;
+           
+          Frame.Navigate(typeof(Dersim));
+          btngit.IsEnabled=false;
+          Combobox.IsEnabled=false;
+          btngit.Visibility=Visibility.Collapsed;
+          btnDil.Background.Opacity=0;
+          btnEdb.Background.Opacity=0;
+        }
+        public Brush Brushes { get; set; }
+    }
+}
